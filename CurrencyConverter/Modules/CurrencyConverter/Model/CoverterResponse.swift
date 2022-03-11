a0ffa734f811b2e0d1a65d708ac5c34754dc6b17
Ahmed Abou-Zeid <ahmedmaika@gmail.com>
2022-03-11 17:54:14 +0200

    stablish currency convert data model

--- CurrencyConverter/Modules/CurrencyConverter/Model/CoverterResponse.swift
+++ CurrencyConverter/Modules/CurrencyConverter/Model/CoverterResponse.swift
@@ -7,10 +7,37 @@
 
 import Foundation
 
-struct CoverterResponse: Decodable {
-    let rates: RateCurrencyData
+struct CurrecnyRatesModel:  Codable {
+    let date: String
+    let base: String
+    let rates: [String: Double]
+    enum CodingKeys: String, CodingKey {
+        case date, base, rates
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        date = try container.decode(String.self, forKey: .date)
+        base = try container.decode(String.self, forKey: .base)
+        rates = try container.decode([String: Double].self, forKey: .rates)
+    }
 }
 
-struct RateCurrencyData: Decodable {
+extension CurrecnyRatesModel {
     
+    func convert(amount: Double, from: String, to: String) -> Double {
+        guard let fromRatioRelativeToBase = rates[from] else {
+            return 0
+        }
+        
+        guard let toRatioRelativeToBase = rates[to] else {
+            return 0
+        }
+        
+        let valueRelativeToBase = amount / fromRatioRelativeToBase // IN EUR
+        
+        return valueRelativeToBase * toRatioRelativeToBase
+    }
 }
+
+
+
